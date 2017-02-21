fc824daa8b622b02393ca149d4ee24747c1c2d59
TehesFR <ths.arborisoft@gmail.com>
2017-02-21 22:00:25 +0100

    set utf8_general_ci

--- 8.0/docker-entrypoint.sh
+++ 8.0/docker-entrypoint.sh
@@ -152,7 +152,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 		file_env 'MYSQL_DATABASE'
 		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | "${mysql[@]}"
 			mysql+=( "$MYSQL_DATABASE" )
 		fi
 
